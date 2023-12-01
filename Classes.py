02fdd1d601f6405b1d829c93e8f034f3a796035d
AdvaithGS <=>
2023-12-01 15:35:35 +0530

    modified Classes.py

--- Classes.py
+++ Classes.py
@@ -1,25 +1,42 @@
 from random import random
-d = {}
 
-class Grouper(str):
- def __init__(self, name : str, d :dict):
-  self.name = name
-  self.d = d
- 
- def ParentGroup(self):
-  return self.d[self.Group_Id]
- 
- def Initiate(self):
-  Group_Id = random.random()
-  while Group_Id in self.d:
-   Group_Id = random.random()
-  self.Group_Id = Group_Id
-  d[self.Group_Id] = [self.name]
- 
- def __str__(self):
-  return self.name
+class Grouper():
+  def __init__(self,d, rigid : bool):
+    self.d = d
+    self.d1 = {}
+    self.rigid = rigid
+    
+  def Associate(self,x, y):
+    ans = None
+    for i in self.d:
+      for j in self.d[i]:
+        if j == x and self.rigid:
+          return 
+        elif j == y:
+          ans = i
+    if ans:
+      self.d[ans].append(x)
+  
+  def Initiate(self,y):
+    for i in self.d:
+      for j in self.d[i]:
+        if j == y:
+          return 
+    key = random()
+    while key in self.d:
+      key = random()
+    self.d[key] = [y]
+  
+  def GetGroup(self, y , include = True):
+    for i in self.d:
+      for j in self.d[i]:
+        if j == y:
+          l = self.d[i]
+          if not include:
+            l.remove()
+          return l
+  def __str__(self):
+    return str([self.d[i] for i in self.d])
+    
 
- def Associate(self, x : Grouper):
-  self.Group_Id = x.Group_Id
-  d[self.Group_Id].append(self.name)
-
+    
