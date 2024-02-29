549deec050ac9ed95ba43cfb1d5022858a9b80da
= <=>
2024-02-29 16:34:42 +0530

    modified anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -1,7 +1,7 @@
 from Classes import Grouper
 from pickle import load
 from termcolor import colored
-
+from random import shuffle
 f = open('anagrams.dat','rb')
 g : Grouper = load(f)
 f.close()
@@ -22,11 +22,16 @@ for n in range(1, int(input("Rounds: ")) + 1):
    correct += 1
    color = 'green'
   except:
-   color = 'red'
+   if(x != '.s'):
+    color = 'red'
   if x == '.q':
-   for i in l:
-    print(i,end = ' ')
-   print()
-   break
+    for i in l:
+     print(i,end = ' ')
+    print()
+    break
+  elif x == '.s':
+    rw = list(rw)
+    shuffle(rw)
+    rw = ''.join(rw)
  else:
   print(colored(f'{n}. ' + rw + f" ({correct}/{total})",'green'))
