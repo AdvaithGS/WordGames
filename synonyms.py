b487a5d182eeb992da6fc593b3bb5b940dcae587
= <=>
2024-02-29 16:35:00 +0530

    Style and synonyms

--- synonyms.py
+++ synonyms.py
@@ -1,5 +1,4 @@
 from random import choice
-import enquiries
 from termcolor import colored 
 from Classes import Grouper
 from pickle import load
@@ -13,13 +12,13 @@ for j in range(3):
     grp  = g.GetGroup(ans := choice(opt))
     grp.remove(ans)
     ques = choice(grp)       
-
-    option = enquiries.choose(ques,opt)
-    
     print(ques)
-
+    for index, o in enumerate(opt):
+        print(f"{index + 1}. {opt[index]}")
+    option = {index+1:o for index,o in enumerate(opt)}[int(input("Choose: "))]
+    
     if option == ans:
-        print(colored('Correct! ','green'))
+        print(colored('\nCorrect! \n','green'))
     else:
         print(colored(f'Wrong, correct answer is {ans}\n', 'red'))
     
